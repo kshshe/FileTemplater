adc9500fb62df1d42848a71589ba53988f850e27
kazakov <kazakov@skytracking.ru>
2018-10-08 11:51:18 +0300

    Обработка событий

--- server/index.js
+++ server/index.js
@@ -3,23 +3,34 @@ var io = require("socket.io")(app);
 var fs = require("fs");
 var serveStatic = require("serve-static");
 var finalhandler = require("finalhandler");
+var serve = serveStatic("build", { index: ["index.html"] });
 
 const APP_PORT = Math.round(Math.random() * 1999 + 8000);
 
-var serve = serveStatic("build", { index: ["index.html"] });
-
 app.listen(APP_PORT, () => {
-  console.log(`App listening at port ${APP_PORT};
-http://localhost:${APP_PORT}`);
+  console.log(`http://localhost:${APP_PORT}`);
 });
 
 function handler(req, res) {
   serve(req, res, finalhandler(req, res));
 }
 
+const methods = require("./methods");
+
 io.on("connection", function(socket) {
-  socket.emit("news", { hello: "world" });
-  socket.on("my other event", function(data) {
-    console.log(data);
+  var onevent = socket.onevent;
+  socket.onevent = function(packet) {
+    var args = packet.data || [];
+    onevent.call(this, packet);
+    packet.data = ["*"].concat(args);
+    onevent.call(this, packet);
+  };
+
+  socket.on("*", function(event, data) {
+    if (methods[event]) {
+      methods[event](data, result => {
+        socket.emit(event + "_result", result);
+      });
+    }
   });
 });
